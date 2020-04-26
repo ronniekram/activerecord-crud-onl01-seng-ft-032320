6f766b4fd60b0d993f78d327f47559431297c97b
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-26 23:21:26 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -78,7 +78,7 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  movie = Movie.update(title: "What, huh?")
+  Movie.update(title: "What, huh?")
 end
 
 def can_update_multiple_items_at_once
